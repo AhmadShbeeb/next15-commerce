46162ef36432c4e5b031c7a43304d051f093a1db
ahmad-shbeeb <a.shbeeb@it-plus24.com>
2024-11-18 23:41:46 +0300

    add persistor to redux

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -1,14 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cartSlice';
 import { useSelector as useBaseSelector } from 'react-redux';
 import { TypedUseSelectorHook } from 'react-redux';
+import { persistStore, persistReducer, PERSIST, PAUSE, FLUSH, REHYDRATE, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const reducers = combineReducers({
+  cart: cartReducer,
+});
 
 export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export const useSelector: TypedUseSelectorHook<RootState> = useBaseSelector;
+export const persistor = persistStore(store);
